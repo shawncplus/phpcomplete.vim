shawncplus/phpcomplete.vim

commit 0bf220f9d350ba72e84c2d61c3dd48b452c513cb

Make subcontext extraction stop at parents
Fixes #32

autoload/phpcomplete.vim
@@ -992,7 +992,7 @@ function! phpcomplete#GetSubContext(context) " {{{
 			continue
 		endif
 		" chars that should stop the variable search
-		if char == '=' || char == ','
+		if char == '=' || char == ',' || char == '(' || char == ')'
 			break
 		endif
 

tests/GetSubContext_test.vim
@@ -10,4 +10,7 @@ fun! TestCase_returns_last_variable_related_part_of_the_context()
 
     let res = phpcomplete#GetSubContext('$foo, $bar, $baz, $bar->')
     call VUAssertEquals('$bar->', res, "should stop at ',' -s")
+
+    let res = phpcomplete#GetSubContext("if ($date->format('N') > 5) $date->")
+    call VUAssertEquals('$date->', res, "should stop at parents")
 endf

commit af2fb0ef2f5cd78ea87d5cded56208a645f9f0dd

Fix regressions introduced in 0bf220f9d350ba72e84c2d61c3dd48b452c513cb
The fix for #32 was handling the problem on a wrong level.
The one line conditional + instruction lines should be broken apart in
GetCurrentInstruction()

autoload/phpcomplete.vim
@@ -974,6 +974,32 @@ function! phpcomplete#GetCurrentInstruction(phpbegin) " {{{
 	endwhile
 	" strip leading whitespace
 	let instruction = substitute(instruction, '^\s\+', '', '')
+
+	" TODO make one liners like: "for ($i=0; $i<42; ++i) $some->" work. Now results in "++$i) $some->"
+
+	" HACK to remove one line conditionals from code like "if ($foo) echo 'bar'"
+	" what the plugin really need is a proper php tokenizer
+	if instruction =~? '^\(if\|while\|foreach\)\s*('
+		" clear everything up until the first (
+		let instruction = substitute(instruction, '^\(if\|while\|foreach\)\s*(\s*', '', '')
+
+		let i = 0
+		let depth = 1
+		while i < len(instruction)
+			if instruction[i] == '('
+				let depth += 1
+			endif
+			if instruction[i] == ')'
+				let depth -= 1
+			endif
+			if depth == 0
+				break
+			end
+			let i += 1
+		endwhile
+		let instruction = instruction[i + 1 : len(instruction)]
+		let instruction = substitute(instruction, '^\s\+', '', '')
+	endif
 	return instruction
 endfunction " }}}
 
@@ -992,7 +1018,7 @@ function! phpcomplete#GetSubContext(context) " {{{
 			continue
 		endif
 		" chars that should stop the variable search
-		if char == '=' || char == ',' || char == '(' || char == ')'
+		if char == '=' || char == ','
 			break
 		endif
 
@@ -1139,7 +1165,7 @@ function! phpcomplete#GetClassName(context, current_namespace, imports) " {{{
 		endif
 
 		"extract the variable name from the context
-		let object = matchstr(a:context, '\s*\zs'.class_name_pattern.'\ze\s*\(::\|->\)')
+		let object = matchstr(phpcomplete#GetSubContext(a:context), '\s*\zs'.class_name_pattern.'\ze\s*\(::\|->\)')
 
 		" scan the file backwards from current line for explicit type declaration (@var $variable Classname)
 		let i = 1 " start from the current line - 1

tests/GetCurrentInstruction_test.vim
@@ -44,5 +44,21 @@ fun! TestCase_returns_instuction_string()
                 \ '$a = "foo"',
                 \ res)
 
+    call cursor(53, 36)
+    let res = phpcomplete#GetCurrentInstruction([1, 1])
+    call VUAssertEquals(
+                \ '$date->',
+                \ res)
+    call cursor(55, 38)
+    let res = phpcomplete#GetCurrentInstruction([1, 1])
+    call VUAssertEquals(
+                \ '$date->',
+                \ res)
+    call cursor(57, 40)
+    let res = phpcomplete#GetCurrentInstruction([1, 1])
+    call VUAssertEquals(
+                \ '$date->',
+                \ res)
+
     silent! bw! %
 endf

tests/GetSubContext_test.vim
@@ -10,7 +10,4 @@ fun! TestCase_returns_last_variable_related_part_of_the_context()
 
     let res = phpcomplete#GetSubContext('$foo, $bar, $baz, $bar->')
     call VUAssertEquals('$bar->', res, "should stop at ',' -s")
-
-    let res = phpcomplete#GetSubContext("if ($date->format('N') > 5) $date->")
-    call VUAssertEquals('$date->', res, "should stop at parents")
 endf

tests/fixtures/GetCurrentInstruction/instructions.php
@@ -46,3 +46,13 @@ class Foo {
 if (true) {
 }
 $a = "foo" // cursor just before the comment
+;
+
+
+
+if( $date->format('N') > 5 ) $date-> // cursor before the comment
+;
+while ($date->format('N') > 5) $date-> // cursor before the comment
+;
+foreach ($date->format('N') > 5) $date-> // cursor before the comment
+;