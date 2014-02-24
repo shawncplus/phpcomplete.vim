shawncplus/phpcomplete.vim

commit 8b376bf7d56b61ec12694d2ea341c16d15582ae5

Improve context detection
Don't remove the last "word" when there were no base found. This helps in
situations where you are after a keyword like "extends" and trigger the
plugin.

autoload/phpcomplete.vim
@@ -202,7 +202,9 @@ function! phpcomplete#CompletePHP(findstart, base) " {{{
 			let b:compl_context = phpcomplete#GetCurrentInstruction(line('.'), col('.') - 2, phpbegin)
 
 			" chop of the "base" from the end of the current instruction
-			let b:compl_context = substitute(b:compl_context, '\s*\$\?\([a-zA-Z_\x7f-\xff][a-zA-Z_0-9\x7f-\xff]*\)*$', '', '')
+			if a:base != ""
+				let b:compl_context = substitute(b:compl_context, '\s*\$\?\([a-zA-Z_\x7f-\xff][a-zA-Z_0-9\x7f-\xff]*\)*$', '', '')
+			end
 
 			return start
 			" We can be also inside of phpString with HTML tags. Deal with
@@ -276,6 +278,9 @@ function! phpcomplete#CompletePHP(findstart, base) " {{{
 	elseif context =~? 'extends'
 		let kinds = context =~? 'class\s' ? ['c'] : ['i']
 		return phpcomplete#CompleteClassName(a:base, kinds, current_namespace, imports)
+	elseif context =~? 'class [a-zA-Z_\x7f-\xff\\][a-zA-Z_0-9\x7f-\xff\\]*'
+		" special case when you've typed the class keyword and the name too, only extends and implements allowed there
+		return filter(['extends', 'implements'], 'stridx(v:val, a:base) == 0')
 	elseif context =~? 'new'
 		return phpcomplete#CompleteClassName(a:base, ['c'], current_namespace, imports)
 	endif

commit 8f17f053e8baaec98d02553f6c53f27aee36f89c

Fix regression introduced in 8b376bf7d5
The a:base is always empty when we are in "findstart" mode, so the base
was never chopped down and prevented completion to recognize $this->some_
contexts.

autoload/phpcomplete.vim
@@ -201,11 +201,6 @@ function! phpcomplete#CompletePHP(findstart, base) " {{{
 			let b:phpbegin = phpbegin
 			let b:compl_context = phpcomplete#GetCurrentInstruction(line('.'), col('.') - 2, phpbegin)
 
-			" chop of the "base" from the end of the current instruction
-			if a:base != ""
-				let b:compl_context = substitute(b:compl_context, '\s*\$\?\([a-zA-Z_\x7f-\xff][a-zA-Z_0-9\x7f-\xff]*\)*$', '', '')
-			end
-
 			return start
 			" We can be also inside of phpString with HTML tags. Deal with
 			" it later (time, not lines).
@@ -227,7 +222,11 @@ function! phpcomplete#CompletePHP(findstart, base) " {{{
 	if exists("b:compl_context")
 		let context = b:compl_context
 		unlet! b:compl_context
-	endif
+		" chop of the "base" from the end of the current instruction
+		if a:base != ""
+			let context = substitute(context, '\s*\$\?\([a-zA-Z_\x7f-\xff][a-zA-Z_0-9\x7f-\xff]*\)*$', '', '')
+		end
+	end
 
 	let [current_namespace, imports] = phpcomplete#GetCurrentNameSpace(getline(0, line('.')))
 