shawncplus/phpcomplete.vim

commit a3872a9899d11de5eccb4870034c75b6d6f565be

Fix built-in classname and interface methods loading
Makes the built-in class names indexed with lowercase names
Makes the methods from interfaces saved with the actual interfacename
instead of a copy-paste induced last classname

autoload/phpcomplete.vim
@@ -2350,7 +2350,7 @@ function! phpcomplete#GetCurrentNameSpace(file_lines) " {{{
 					if has_key(g:php_builtin_classnames, tolower(import.name))
 						let import['kind'] = 'c'
 						let import['builtin'] = 1
-					elseif has_key(g:php_builtin_interfaces, import.name)
+					elseif has_key(g:php_builtin_interfacenames, tolower(import.name))
 						let import['kind'] = 'i'
 						let import['builtin'] = 1
 					else
@@ -2477,7 +2477,7 @@ for [classname, class_info] in items(g:php_builtin_classes)
 		endif
 	endfor
 
-	let g:php_builtin_classnames[class_info.name] = ''
+	let g:php_builtin_classnames[classname] = ''
 	for [method_name, method_info] in items(class_info.methods)
 		let g:php_builtin_object_functions[classname.'::'.method_name.'('] = method_info.signature
 	endfor
@@ -2496,10 +2496,10 @@ for [interfacename, info] in items(g:php_builtin_interfaces)
 
 	let g:php_builtin_interfacenames[interfacename] = ''
 	for [method_name, method_info] in items(class_info.methods)
-		let g:php_builtin_object_functions[classname.'::'.method_name.'('] = method_info.signature
+		let g:php_builtin_object_functions[interfacename.'::'.method_name.'('] = method_info.signature
 	endfor
 	for [method_name, method_info] in items(class_info.static_methods)
-		let g:php_builtin_object_functions[classname.'::'.method_name.'('] = method_info.signature
+		let g:php_builtin_object_functions[interfacename.'::'.method_name.'('] = method_info.signature
 	endfor
 endfor
 

tests/GetCurrentNameSpace_test.vim
@@ -33,14 +33,14 @@ fun! TestCase_returns_imported_namespaces_and_classes_with_their_info_from_tags(
     let [namespace, imports] = phpcomplete#GetCurrentNameSpace(file_lines)
     call VUAssertEquals({
                 \ 'Foo': {'cmd': '/^class Foo {$/', 'static': 0, 'name': 'Foo', 'namespace': 'NS1', 'kind': 'c', 'builtin': 0, 'filename': 'fixtures/common/namespaced_foo.php'},
-                \ 'ArrayAccess': {'name': 'ArrayAccess', 'kind': ''},
-                \ 'AO': {'name': 'ArrayObject', 'kind': ''},
-                \ 'DateTimeZone': {'name': 'DateTimeZone', 'kind': ''},
-                \ 'LE': {'name': 'LogicException', 'kind': ''},
-                \ 'DateTime': {'name': 'DateTime', 'kind': ''},
+                \ 'ArrayAccess': {'name': 'ArrayAccess', 'kind': 'i', 'builtin': 1},
+                \ 'AO': {'name': 'ArrayObject', 'kind': 'c', 'builtin': 1},
+                \ 'DateTimeZone': {'name': 'DateTimeZone', 'kind': 'c', 'builtin': 1},
+                \ 'LE': {'name': 'LogicException', 'kind': 'c', 'builtin': 1},
+                \ 'DateTime': {'name': 'DateTime', 'kind': 'c', 'builtin': 1},
                 \ 'SUBNS': {'cmd': '/^namespace NS1\\SUBNS;$/', 'static': 0, 'name': 'NS1\SUBNS', 'kind': 'n', 'builtin': 0, 'filename': 'fixtures/common/namespaced_foo.php'},
-                \ 'EE': {'name': 'ErrorException', 'kind': ''},
-                \ 'E': {'name': 'Exception', 'kind': ''}},
+                \ 'EE': {'name': 'ErrorException', 'kind': 'c', 'builtin': 1},
+                \ 'E': {'name': 'Exception', 'kind': 'c', 'builtin': 1}},
                 \ imports)
 
     " with old style tags, no namespace matches will be returned and the first
@@ -50,13 +50,13 @@ fun! TestCase_returns_imported_namespaces_and_classes_with_their_info_from_tags(
     let [namespace, imports] = phpcomplete#GetCurrentNameSpace(file_lines)
     call VUAssertEquals({
                 \ 'Foo': {'cmd': '/^class Foo {$/', 'static': 0, 'name': 'Foo', 'kind': 'c', 'builtin': 0, 'filename': 'fixtures/common/fixtures/common/namespaced_foo.php'},
-                \ 'ArrayAccess': {'name': 'ArrayAccess', 'kind': ''},
-                \ 'AO': {'name': 'ArrayObject', 'kind': ''},
-                \ 'DateTimeZone': {'name': 'DateTimeZone', 'kind': ''},
-                \ 'LE': {'name': 'LogicException', 'kind': ''},
-                \ 'DateTime': {'name': 'DateTime', 'kind': ''},
+                \ 'ArrayAccess': {'name': 'ArrayAccess', 'kind': 'i', 'builtin': 1},
+                \ 'AO': {'name': 'ArrayObject', 'kind': 'c', 'builtin': 1},
+                \ 'DateTimeZone': {'name': 'DateTimeZone', 'kind': 'c', 'builtin': 1},
+                \ 'LE': {'name': 'LogicException', 'kind': 'c', 'builtin': 1},
+                \ 'DateTime': {'name': 'DateTime', 'kind': 'c', 'builtin': 1},
                 \ 'SUBNS': {'name': 'SUBNS', 'namespace': 'NS1', 'kind': '', 'builtin': 0},
-                \ 'EE': {'name': 'ErrorException', 'kind': ''},
-                \ 'E': {'name': 'Exception', 'kind': ''}},
+                \ 'EE': {'name': 'ErrorException', 'kind': 'c', 'builtin': 1},
+                \ 'E': {'name': 'Exception', 'kind': 'c', 'builtin': 1}},
                 \ imports)
 endf

commit 2fbd719cf574e531f966e276cb4dbc056f96784c

Normalize the case around builtin classes and interfaces
All hashes should be indexed lowercase, the suggestions should be provided
from under the `.name` key. This fixes the all lowercase suggestion bug
introduced in a3872a9899d11de5eccb4870034c75b6d6f565be

autoload/phpcomplete.vim
@@ -348,11 +348,11 @@ function! phpcomplete#CompleteUse(base) " {{{
 	if base !~ '\'
 		let builtin_classnames = filter(keys(copy(g:php_builtin_classnames)), 'v:val =~? "^'.classname_match_pattern.'"')
 		for classname in builtin_classnames
-			call add(res, {'word': classname, 'kind': 'c'})
+			call add(res, {'word': g:php_builtin_classes[tolower(classname)].name, 'kind': 'c'})
 		endfor
 		let builtin_interfacenames = filter(keys(copy(g:php_builtin_interfacenames)), 'v:val =~? "^'.classname_match_pattern.'"')
 		for interfacename in builtin_interfacenames
-			call add(res, {'word': interfacename, 'kind': 'i'})
+			call add(res, {'word': g:php_builtin_interfaces[tolower(interfacename)].name, 'kind': 'i'})
 		endfor
 	endif
 
@@ -542,7 +542,7 @@ function! phpcomplete#CompleteGeneral(base, current_namespace, imports) " {{{
 		" Add builtin class names
 		for [classname, info] in items(g:php_builtin_classnames)
 			if classname =~? '^'.base
-				let builtin_classnames[leading_slash.classname] = info
+				let builtin_classnames[leading_slash.g:php_builtin_classes[tolower(classname)].name] = info
 			endif
 		endfor
 		for [interfacename, info] in items(g:php_builtin_interfacenames)
@@ -915,7 +915,7 @@ function! phpcomplete#CompleteClassName(base, kinds, current_namespace, imports)
 				if has_key(g:php_builtin_classes[tolower(classname)].methods, '__construct')
 					let menu = g:php_builtin_classes[tolower(classname)]['methods']['__construct']['signature']
 				endif
-				call add(res, {'word': leading_slash.classname, 'kind': 'c', 'menu': menu})
+				call add(res, {'word': leading_slash.g:php_builtin_classes[tolower(classname)].name, 'kind': 'c', 'menu': menu})
 			endfor
 		endif
 

tests/CompleteGeneral_test.vim
@@ -139,8 +139,17 @@ fun! TestCase_completes_builtin_class_names() " {{{
 
     " PDO should not be picked up
     let g:php_builtin_classnames = {
-                \ 'DateTime':'',
-                \ 'PDO':'',
+                \ 'datetime':'',
+                \ 'pdo':'',
+                \ }
+
+    let g:php_builtin_classes = {
+                \ 'datetime':{
+                \   'name': 'DateTime',
+                \ },
+                \ 'pdo':{
+                \   'name': 'PDO',
+                \ }
                 \ }
 
     let res = phpcomplete#CompleteGeneral('date', '\', {})
@@ -154,7 +163,12 @@ fun! TestCase_completes_builtin_interface_names() " {{{
 
     " PDO should not be picked up
     let g:php_builtin_interfacenames = {
-                \ 'Traversable':'',
+                \ 'traversable':'',
+                \ }
+    let g:php_builtin_interfaces = {
+                \ 'traversable':{
+                \   'name': 'Traversable',
+                \ }
                 \ }
 
     let res = phpcomplete#CompleteGeneral('Tr', '\', {})
@@ -233,8 +247,16 @@ fun! TestCase_completes_builtin_class_names_when_in_namespace_and_base_starts_wi
 
     " PDO should not be picked up
     let g:php_builtin_classnames = {
-                \ 'DateTime':'',
-                \ 'PDO':'',
+                \ 'datetime':'',
+                \ 'pdo':'',
+                \ }
+    let g:php_builtin_classes = {
+                \ 'datetime':{
+                \   'name': 'DateTime',
+                \ },
+                \ 'pdo':{
+                \   'name': 'PDO',
+                \ }
                 \ }
 
     let res = phpcomplete#CompleteGeneral('\date', 'SomeNameSpace', {})

tests/CompleteUse_test.vim
@@ -14,8 +14,17 @@ endf
 fun! TestCase_completes_builtin_classes()
     call SetUp()
     let g:php_builtin_classnames = {
-                \ 'ArrayAccess': '',
-                \ 'ArrayObject': '',
+                \ 'arrayaccess': '',
+                \ 'arrayobject': '',
+                \ }
+
+    let g:php_builtin_classes = {
+                \ 'arrayaccess':{
+                \   'name': 'ArrayAccess',
+                \ },
+                \ 'arrayobject':{
+                \   'name': 'ArrayObject',
+                \ },
                 \ }
 
     " returns every builtin class when nothing typed in

tests/GetClassLocation_test.vim
@@ -9,7 +9,7 @@ endf
 fun! TestCase_return_VIMPHP_BUILTINOBJECT_when_classname_in_builtin_classes()
     call SetUp()
 
-    let g:php_builtin_classnames = {'DateTime': ''}
+    let g:php_builtin_classnames = {'datetime': ''}
     let g:php_builtin_classes = {
                 \'datetime':{}
                 \}

tests/LoadData_test.vim
@@ -1,0 +1,24 @@
+fun! SetUp()
+
+endf
+fun! TestCase_creates_various_global_hashes()
+    call phpcomplete#LoadData()
+
+    call VUAssertTrue(exists('g:php_builtin_classes'))
+    call VUAssertTrue(exists('g:php_builtin_classnames'))
+    call VUAssertTrue(exists('g:php_builtin_interfaces'))
+    call VUAssertTrue(exists('g:php_builtin_interfacenames'))
+    call VUAssertTrue(exists('g:php_builtin_functions'))
+    call VUAssertTrue(exists('g:php_builtin_vars'))
+endf
+
+fun! TestCase_php_built_classnames_and_builtin_classes_are_indexed_lowercase()
+    call phpcomplete#LoadData()
+
+    call VUAssertTrue(has_key(g:php_builtin_classes, 'datetime'))
+    call VUAssertTrue(has_key(g:php_builtin_classnames, 'datetime'))
+    call VUAssertTrue(has_key(g:php_builtin_interfaces, 'traversable'))
+    call VUAssertTrue(has_key(g:php_builtin_interfacenames, 'traversable'))
+endf
+
+" vim: foldmethod=marker:expandtab:ts=4:sts=4