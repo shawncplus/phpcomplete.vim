cbd0e799c0aa7792c40724cff87e9e4f3bb7b6f6
dudu <complex857@gmail.com>
2015-01-18 20:19:48 +0100

    Make running tests faster and easier
    Added a vimrc only for testing, so the environment is controlled and vim
    won't load your usual plugins and everything while executing tests.
    Also made it self-download its vimunit plugin dependency

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-VU='../../vimunit/vutest.sh'
+set -eu
 
-if [ ! -f $VU ]; then
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+if [ -f "$DIR/../vimunit/vutest.sh" ]; then
+	# vimunit in the plugin root
+	VU="$DIR/../vimunit/vutest.sh"
+
+elif [ -f "$DIR/../../vimunit/vutest.sh" ]; then
+	# vimunit in the plugin root's parent dir (think of ~/.vim/bundle)
+	VU="$DIR/../../vimunit/vutest.sh"
+
+else
+	# no vimunit found, just grab it from github
+    git clone https://github.com/dsummersl/vimunit.git "$DIR/../vimunit"
+	VU="$DIR/../vimunit/vutest.sh"
+fi
+
+if [ ! -f "$VU" ]; then
 	echo "Could not run tests. Vimunit executeable not found at: '$VU'"
 else
     if [[ $# > 0 ]]; then
         for f in $@; do
-            $VU $f
+            $VU -e "vim -u $DIR/vimrc" $f
         done
     else
-        for f in ./*.vim; do
-            $VU $f
+        for f in "$DIR/"*.vim; do
+            $VU -e "vim -u $DIR/vimrc"  $f
         done
     fi
 fi
